8033e89b3e4c38478443e3ee12d687b1d7561950
Shlok Motwani <shlok.codes@gmail.com>
2025-01-02 17:11:00 +0530

    read and display file contents based on URL

---
+++ index.js
@@ -0,0 +1,25 @@
+let http = require("http");
+let fs = require("fs");
+let url = require("url");
+
+http
+  .createServer((req, res) => {
+    let parsedURL = url.parse(req.url, true);
+    let filename;
+    if (parsedURL.pathname === "/") {
+      filename = "./index.html";
+    }
+    else{
+        filename = "./" + parsedURL.pathname.toString() + ".html";
+    }
+    fs.readFile(filename, (err, data) => {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        return res.end("404 Not Found");
+      }
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(9004);
